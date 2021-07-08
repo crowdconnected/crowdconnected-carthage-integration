2e0537415d2e32eb7bb961c732d9b09bb8cef815
Robert Tanase <robert.tanase97@gmail.com>
2021-07-08 10:50:45 +0300

    Initial commit

---
+++ TestCarthageIntegration/TestCarthageIntegrationApp.swift
@@ -0,0 +1,12 @@
+//  Created by TCode on 8/7/21.
+
+import SwiftUI
+
+@main
+struct TestCarthageIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
